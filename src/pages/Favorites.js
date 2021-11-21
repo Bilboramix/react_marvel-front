2c451ad946d1537894fa125439e8a8e1fae652cd
bilbo <axel.catois@gmail.com>
2021-11-21 23:42:27 +0100

    add remove favorite button

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -23,7 +23,42 @@ const Favorites = ({ setCanSearch }) => {
 
       setIsLoading(false);
     }
-  }, [setCanSearch, navigate, favChars, favComics]);
+  }, [setCanSearch, navigate]);
+
+  const deleteFavChar = (fav) => {
+    const newCookie = Cookies.get("fav-char").split(",");
+    newCookie.splice(newCookie.indexOf(fav), 1);
+    Cookies.remove("fav-char");
+    Cookies.remove(`${fav}`);
+    if (newCookie.length > 0) {
+      console.log("pas vide");
+      Cookies.set("fav-char", newCookie);
+      setFavChars(Cookies.get("fav-char").split(","));
+    } else {
+      setFavChars();
+    }
+    if (Cookies.get("fav-char") === undefined && Cookies.get("fav-comic") === undefined) {
+      navigate("/");
+    }
+  };
+
+  const deleteFavComic = (fav) => {
+    const newCookie = Cookies.get("fav-comic").split(",");
+    newCookie.splice(newCookie.indexOf(fav), 1);
+    Cookies.remove("fav-comic");
+    Cookies.remove(`${fav}`);
+    if (newCookie.length > 0) {
+      console.log("pas vide");
+      Cookies.set("fav-comic", newCookie);
+      setFavComics(Cookies.get("fav-comic").split(","));
+    } else {
+      setFavComics();
+    }
+    if (Cookies.get("fav-char") === undefined && Cookies.get("fav-comic") === undefined) {
+      navigate("/");
+    }
+  };
+
   return isLoading ? (
     <section className="container">Loading</section>
   ) : (
@@ -33,9 +68,16 @@ const Favorites = ({ setCanSearch }) => {
           <h2>Personnages favoris</h2>
           {favChars.map((fav, index) => {
             return (
-              <div className="card">
+              <div key={index} className="card">
                 <h3>{fav}</h3>
                 <img src={Cookies.get(`${fav}`)} alt={fav} />
+                <button
+                  onClick={() => {
+                    deleteFavChar(fav);
+                  }}
+                >
+                  Retirer des favoris
+                </button>
               </div>
             );
           })}
@@ -47,9 +89,16 @@ const Favorites = ({ setCanSearch }) => {
           <h2>Comics favoris</h2>
           {favComics.map((fav, index) => {
             return (
-              <div className="card">
+              <div key={index} className="card">
                 <h3>{fav}</h3>
                 <img src={Cookies.get(`${fav}`)} alt={fav} />
+                <button
+                  onClick={() => {
+                    deleteFavComic(fav);
+                  }}
+                >
+                  Retirer des favoris
+                </button>
               </div>
             );
           })}
